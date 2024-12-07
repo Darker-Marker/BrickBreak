ab64524b4b22894fa588ceee86e92202b72fb05a
Darker-Marker <95483689+Darker-Marker@users.noreply.github.com>
2024-12-07 05:23:40 +0000

    commit -m “Submission 12/6/2024”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,104 @@
 import java.awt.*;
-
-public class Ball {
+//  Class author:  Parker Lasko
+//  Date created:  12/4/2024
+//  General description: is blueprint for ball object providing its possible actions and associated variables 
+public class Ball{
 	//your code here!
 	
 	//don't forget you need instance variables:
+	private int XPos;
+	private int YPos;
+	private int size;
+	private int XVelocity;
+	private int YVelocity;
+
+
 	
+
 	
 	//constructor(s):
+	public Ball(int XPos, int YPos, int size) {
+		this.XPos = XPos;
+		this.YPos = YPos;
+		this.size = size;
+		this.XVelocity = 1;
+		this.YVelocity = 1;
 	
+	
+	}
+	//getters for ball
+
+
+public int getXpos()
+{
+	return XPos;
+}
+public int getYpos() 
+{
+	return YPos;
+}
+public int getSize()
+{
+	return size;
+}
+
+	//setters
+	public void setX(int XPos)
+	 {
+		this.XPos = XPos;
+	}
+	public void setY(int YPos)
+	 {
+		this.YPos = YPos;
+	}
+	public void setsize(int size)
+	{
+	   this.size = size;
+   }
+   public void setXVelocity(int XVelocity)
+   {
+	  this.XVelocity = XVelocity;
+  }
+  public void setYVelocity(int YVelocity)
+  {
+	 this.YVelocity = YVelocity;
+ }
+
+
+
 	//methods:
 
+	//  Pre-condition: must provide locatiion of ball and its size
+	//  possible limitations on input: none
+//  Post-condition: will draw a white oval at the location of the ball 
+
+
+public void draw(Graphics g)
+{
+ g.setColor(Color.white); // call first to set the desired color
+	   g.fillOval(XPos, YPos, size, size); // draws an oval (ball)
+
+}
+//  Pre-condition: must provide XVelocity, YVelocity, XPos, and YPos
+	//  possible limitations on input: none
+//  Post-condition: Moves the ball 
+public void move()
+{
+	XPos += XVelocity;
+	YPos += YVelocity;
+}
+//  Pre-condition: must provide XVelocity 
+	//  possible limitations on input: none
+//  Post-condition: will reverse the velocity of the ball sending it in the oppiste x direction
+public void reverseX()
+{
+	XVelocity = XVelocity * -1;
+}
+//  Pre-condition: must provide YVelocity 
+	//  possible limitations on input: none
+//  Post-condition: will reverse the velocity of the ball sending it in the oppiste y direction
+public void reverseY()
+{
+	YVelocity = YVelocity * -1;
+}
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,110 @@
-import java.awt.Graphics;
 import java.awt.*;
-
+//  Class author:  Parker Lasko
+//  Date created:  12/4/2024
+//  General description: Creates a blue print for the paddle providing its possible actions and associated variables 
 public class Paddle {
 
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private int width;
+    private int height;
+	private int Velocity;
 	
 	//constructor(s):
+	public Paddle (int x, int y, int width, int height)
+{
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.Velocity = 0;
 	
+}
+	//setters for paddle
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void setwidth(int width) {
+		this.width = width;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public void setVelocity(int Velocity) {
+		this.Velocity = Velocity;
+	}
+
+	//getters for paddle
+	public int getX()
+	{
+return x;
+	}
+	public int getY()
+	{
+return y;
+	}
+	public int getWidth()
+	{
+return width;
+	}
+	public int getHeight()
+	{
+return height;
+	}
+	public int getVelocity()
+	{
+return Velocity;
+	}
+
 	//methods:
-	
-	
+//  Pre-condition: must provide locatiion of paddle and its size (width and height)
+//  possible limitations on input: none
+//  Post-condition: will draw a white rectangle at the location of the paddle 
+	public void draw (Graphics g)
+	{
+		 g.setColor(Color.white); // call first to set the desired color
+               g.fillRect(x ,y ,width ,height); // draws a rectangle (paddle)
+	}
+	//  Pre-condition: must provide Velocity and x 
+	//  possible limitations on input: none
+//  Post-condition: Moves the rectangle and prevnts it from going outside of the set bounds of the screen (x = 0,600 ) 
+	public void move ()
+	{
+		if (Velocity > 0)
+		{
+			Velocity--;
+		}
+		if (Velocity < 0)
+		{
+			Velocity++;
+		}
+		if(x < 0)
+		{
+			x = 0;
+		
+		}
+		else if(x > 600)
+		{
+			x = 600;
+			
+		}
+		else{
+			x += Velocity;
+		}
+		
+	}
+		//  Pre-condition: must provide Velocity and uses this.Velocity
+	//  possible limitations on input: none
+//  Post-condition: adds velocity to the paddle based on the current velocity and the velocity that sahould be added based on the inputted int Velocity when the method is called
+	public void addVelocity (int Velocity)
+	{
+		this.Velocity += Velocity;
+	}
 }
